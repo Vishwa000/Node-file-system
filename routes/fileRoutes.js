26b6cf674b41bc01f4e27d2551c4a9d4c49f25b2
Vishwa S <113334237+Vishwa000@users.noreply.github.com>
2024-02-28 00:23:36 +0530

    update route file

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fileController = require('../controllers/fileController');
 
+router.get('/', (req, res) => {res.redirect('/all');});
 router.post('/create', fileController.createFile);
 router.get('/all', fileController.getAllFiles);
 
